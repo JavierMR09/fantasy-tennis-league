7bbb80b2c2e213d7f77b5011fa535a4bd40e3b77
Javier Millan Reyes <156706893+JavierMR09@users.noreply.github.com>
2025-06-11 16:14:44 -0400

    fix: add missing quote in res.send()

--- apps/api/index.js
+++ apps/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
-res.send("Hello from Fantasy Tennis API!);
+res.send("Hello from Fantasy Tennis API!");
 });
 app.listen(port, () => {
 console.log(`API listening on http://localhost:${port}`);
